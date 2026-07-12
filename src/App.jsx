ea6cfb66c99d38b990fb4a21e8fce6e61e6937ea
Artur Freitas <afreitas@efs.at>
2026-07-12 03:42:12 +0100

    Notas e Graduação

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ export default function App() {
       <main>
         <Hero />
         <Gallery />
-        <Timeline />
+        {/* <Timeline /> */}
         <SpecialEvents />
         <Letters />
         <Closing />
